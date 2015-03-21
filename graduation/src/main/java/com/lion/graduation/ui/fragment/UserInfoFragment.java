9c1e50a7f6f03e3fe2482780ff76c3d556570da7
MyLifeMyTravel <1025263614lsj@gmail.com>
2015-03-21 09:26:25 +0800

    add TaskFragment

---
+++ graduation/src/main/java/com/lion/graduation/ui/fragment/UserInfoFragment.java
@@ -0,0 +1,30 @@
+package com.lion.graduation.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 用户信息显示
+ * Created by Lion on 2015/3/20.
+ */
+public class UserInfoFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
