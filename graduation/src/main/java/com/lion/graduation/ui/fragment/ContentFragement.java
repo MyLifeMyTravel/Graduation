6aafeeacabdb12e69563952e356b4c9224215c7a
MyLifeMyTravel <1025263614lsj@gmail.com>
2015-03-21 09:25:56 +0800

    添加Item点击事件

--- graduation/src/main/java/com/lion/graduation/ui/fragment/ContentFragement.java
+++ graduation/src/main/java/com/lion/graduation/ui/fragment/ContentFragement.java
@@ -3,12 +3,12 @@ package com.lion.graduation.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,8 +17,9 @@ import com.amap.api.location.AMapLocalWeatherListener;
 import com.amap.api.location.AMapLocalWeatherLive;
 import com.amap.api.location.LocationManagerProxy;
 import com.lion.graduation.R;
-import com.lion.graduation.model.TaskItem;
-import com.lion.graduation.ui.adapter.TaskItemAdapter;
+import com.lion.graduation.model.TaskItemModel;
+import com.lion.graduation.ui.adapter.BaseRecyclerViewAdapter;
+import com.lion.graduation.ui.adapter.TaskRecyclerViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,52 +28,70 @@ import java.util.List;
  * 开启应用显示的界面
  * Created by Lion on 2015/3/13.
  */
-public class ContentFrame extends Fragment {
+public class ContentFragement extends Fragment {
 
-    //
-    private LinearLayout weatherLayout = null;
-    private TextView weather_content = null;
-    //巡检信息显示列表
-    private ListView mListView = null;
-    private List<TaskItem> taskItems = null;
+    private List<TaskItemModel> items = null;
+
+    //RecylerView对象
+    private RecyclerView mRecyclerView;
+    private TaskRecyclerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    //Weather模块的CardView
+    private CardView weather_Card = null;
+    private TextView weather_Content = null;
 
     //定位服务对象
     private LocationManagerProxy mLocationManagerProxy = null;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initTaskData();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.content_layout, container, false);
+        initWeatherCard(view);
 
-        weatherLayout = (LinearLayout) view.findViewById(R.id.weather);
-        weather_content = (TextView) weatherLayout.findViewById(R.id.weather_content);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.drawer_content);
 
-        mListView = (ListView) view.findViewById(R.id.task_list);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-        initTaskData();
-        mListView.setAdapter(new TaskItemAdapter(taskItems));
-        //设置ListView的Item点击事件
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // specify an adapter (see also next example)
+        mAdapter = new TaskRecyclerViewAdapter(items);
+        mAdapter.setOnItemClickListener(new BaseRecyclerViewAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+            public void onItemClick(View v, int position) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new TaskFragment()).addToBackStack(null).commit();
+                Toast.makeText(getActivity(), position + "", Toast.LENGTH_LONG).show();
             }
         });
+
+        mRecyclerView.setAdapter(mAdapter);
         return view;
     }
 
+    private void initWeatherCard(View view) {
+        weather_Card = (CardView) view.findViewById(R.id.weather);
+        weather_Content = (TextView) weather_Card.findViewById(R.id.weather_content);
+    }
+
     private void initTaskData() {
-        taskItems = new ArrayList<>();
-        TaskItem item = null;
+        items = new ArrayList<>();
+        TaskItemModel item = null;
         String[] task_name = getResources().getStringArray(R.array.task_name);
         String[] task_sign = getResources().getStringArray(R.array.task_sign);
         String[] task_status = getResources().getStringArray(R.array.task_status);
 
         for (int i = 0; i < task_name.length; i++) {
-            item = new TaskItem();
+            item = new TaskItemModel();
             item.setTask_name(task_name[i]);
             item.setTask_sign(task_sign[i]);
             item.setTask_status(task_status[i]);
-            taskItems.add(item);
+            items.add(item);
         }
     }
 
@@ -98,8 +117,7 @@ public class ContentFrame extends Fragment {
                 String windPower = aMapLocalWeatherLive.getWindPower();//风力
                 String humidity = aMapLocalWeatherLive.getHumidity();//空气湿度
                 String reportTime = aMapLocalWeatherLive.getReportTime();//数据发布时间
-                weather_content.setText("城市:" + city + "\n天气情况:" + weather + "\n风向:" + windDir + "\n风力:" + windPower + "\n空气湿度:" + humidity + "\n发布时间:" + reportTime);
-                Toast.makeText(getActivity(), "城市:" + city + "\n天气情况:" + weather + "\n风向:" + windDir + "\n风力:" + windPower + "\n空气湿度:" + humidity + "\n发布时间:" + reportTime, Toast.LENGTH_LONG).show();
+                weather_Content.setText("城市:" + city + "\n天气情况:" + weather + "\n风向:" + windDir + "\n风力:" + windPower + "\n空气湿度:" + humidity + "\n发布时间:" + reportTime);
             } else {
                 // 获取实时天气失败
                 Toast.makeText(getActivity(), "获取天气预报失败:" + aMapLocalWeatherLive.getAMapException().getErrorMessage(), Toast.LENGTH_SHORT).show();
