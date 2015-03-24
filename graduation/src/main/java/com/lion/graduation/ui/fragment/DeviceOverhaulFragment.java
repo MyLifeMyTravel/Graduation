f199d9b3f43dd0bbf57fea1f111ae4b067077ec8
MyLifeMyTravel <1025263614lsj@gmail.com>
2015-03-24 19:37:37 +0800

    初始添加设备巡检Fragement

---
+++ graduation/src/main/java/com/lion/graduation/ui/fragment/DeviceOverhaulFragment.java
@@ -0,0 +1,10 @@
+package com.lion.graduation.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 设备检修界面
+ * Created by Lion on 2015/3/23.
+ */
+public class DeviceOverhaulFragment extends Fragment {
+}
