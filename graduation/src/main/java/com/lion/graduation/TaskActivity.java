18ec2d95a19f5fcff9e8e68762ce98f2035cc8fb
MyLifeMyTravel <1025263614lsj@gmail.com>
2015-03-24 19:10:07 +0800

    添加TaskActivity，用于打开巡检任务时遮盖抽屉菜单，防止误操作

---
+++ graduation/src/main/java/com/lion/graduation/TaskActivity.java
@@ -0,0 +1,9 @@
+package com.lion.graduation;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Lion on 2015/3/24.
+ */
+public class TaskActivity extends ActionBarActivity {
+}
